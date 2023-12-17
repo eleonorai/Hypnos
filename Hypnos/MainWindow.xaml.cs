ef4307be6fa086b3e4689ccadcf145aa49d8db78
ODESI <ODESI@LAPTOP-8HJ85GUS>
2023-12-17 17:22:00 +0200

    Добавьте файлы проекта.

---
+++ Hypnos/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Hypnos
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+        double t = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            t += 0.08;
+            image.RenderTransform = new System.Windows.Media.RotateTransform() { Angle = t };
+            if (t >= 360)
+                t = 0;
+        }
+    }
+}
